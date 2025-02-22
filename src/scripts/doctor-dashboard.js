9170cf74ad72897e9ffe1956269cfabb30039673
Ivan <good.day.2003@mail.ru>
2025-02-22 23:30:35 +0300

    Update doctor-dashboard.js

--- src/scripts/doctor-dashboard.js
+++ src/scripts/doctor-dashboard.js
@@ -25,7 +25,7 @@ document.querySelectorAll('.sidebar nav ul li a').forEach(link => {
 // Выход из системы
 document.getElementById('logout').addEventListener('click', () => {
     localStorage.removeItem('token'); // Удаляем токен
-    window.location.href = '/doctor-login.html'; // Перенаправляем на страницу авторизации
+    window.location.href = 'https://emptyyyyyyy.github.io/IAS/src/html/doctor.html'; // Перенаправляем на страницу авторизации
 });
 
 // Генерация списка пациентов
