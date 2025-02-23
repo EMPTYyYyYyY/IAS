7ff784a11c5f9132db08abd418410d921b630271
Ivan <good.day.2003@mail.ru>
2025-02-24 02:40:19 +0300

    Update patient-dashboard.js

--- src/scripts/patient-dashboard.js
+++ src/scripts/patient-dashboard.js
@@ -140,7 +140,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const logoutBtn = document.getElementById('logout');
         if (logoutBtn) {
             logoutBtn.addEventListener('click', () => {
-                window.location.href = '/logout';
+                window.location.href = 'index.html';
             });
         }
     };
