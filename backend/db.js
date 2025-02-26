5f4ebfc192103a19c0b6b96f78875702d3a48235
Ivan <good.day.2003@mail.ru>
2025-02-26 23:29:55 +0300

    f

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Настройка подключения к SQLite
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite', // Файл базы данных
-});
-
-module.exports = sequelize;
