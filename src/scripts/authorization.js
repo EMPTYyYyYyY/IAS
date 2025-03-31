16a906975294b30c50e3974d8d43bc0d0d6edc3b
Ivan <good.day.2003@mail.ru>
2025-03-31 14:57:53 +0300

    Update authorization.js

--- src/scripts/authorization.js
+++ src/scripts/authorization.js
@@ -1,5 +1,5 @@
 const BASE_URL = "__BASE_URL__";
-alert(BASE_URL)
+alert(JSON.stringify(BASE_URL))
 document.querySelector('.login-form form').addEventListener('submit', async function(e) {
     e.preventDefault();
     
