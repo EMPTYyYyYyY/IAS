51785c28df4beb925e35ffc29c0cdef3a950c5b1
EMPTYyYyYyY <EMPTYyYyYyY@users.noreply.github.com>
2025-03-31 17:42:42 +0000

    deploy: 0ce8a28bff819c193543d0cdaba3d2efbebca25d

--- src/scripts/doctor-dashboard.js
+++ src/scripts/doctor-dashboard.js
@@ -128,6 +128,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         headers: {
                             'Content-Type': 'application/json'
                         },
+                        credentials: 'include',
                         body: JSON.stringify(requestData)
                     });
                     
